5f6b7c49d79930b4d529ef7e5ce90251bab90573
Abhinav <137620031+abhinav-ratan@users.noreply.github.com>
2026-02-11 03:22:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,32 +3,29 @@ const config = window.VALENTINE_CONFIG;
 document.getElementById("title").innerText =
 config.valentineName + ", my love";
 
-function nextPage(pageNumber) {
+function nextPage(n){
 
 document.querySelectorAll(".page")
-.forEach(p => p.classList.add("hidden"));
+.forEach(p=>p.classList.add("hidden"));
 
-document.getElementById("page" + pageNumber)
+document.getElementById("page"+n)
 .classList.remove("hidden");
 
 }
 
-function moveButton(btn) {
+function moveButton(btn){
 
-const x = Math.random() * 80;
-const y = Math.random() * 80;
-
-btn.style.position = "absolute";
-btn.style.left = x + "%";
-btn.style.top = y + "%";
+btn.style.position="absolute";
+btn.style.left=Math.random()*80+"%";
+btn.style.top=Math.random()*80+"%";
 
 }
 
-const meter = document.getElementById("loveMeter");
-const value = document.getElementById("loveValue");
+const meter=document.getElementById("meter");
+const value=document.getElementById("value");
 
 if(meter){
-meter.oninput = () => {
-value.innerText = meter.value + "%";
+meter.oninput=()=>{
+value.innerText=meter.value+"%";
 };
 }
