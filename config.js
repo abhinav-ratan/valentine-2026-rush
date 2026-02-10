89400d3db9f4b46b22a6b8b154612d920e938050
Abhinav <137620031+abhinav-ratan@users.noreply.github.com>
2026-02-11 03:19:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,48 +1,17 @@
 const CONFIG = {
 
-    valentineName: "Rush",
+    valentineName: "Rashmi",
 
     pageTitle: "Will you be my valentine?",
-
-    questions: {
-
-        first: {
-            text: "Will you be my valentine this year?",
-            yesBtn: "Yes",
-            noBtn: "No"
-        },
-
-        second: {
-            text: "Will you be my valentine forever?",
-            yesBtn: "Yes",
-            noBtn: "No"
-        },
-
-        meter: {
-            text: "How much do you love me?",
-            startText: "This much!",
-            nextBtn: "Continue"
-        }
-    },
-
-    celebration: {
-        title: "Rashmi, mon petit cherie",
-
-        message:
-`I thought it mattered what I said or where I said it. Then I realized the only thing that matters is that you, you make me happier than I ever thought I could be, and if you let me, I will spend the rest of my life trying to make you feel the same way.
-
-I love you Rashmi <3, you are my one!
-
-I can't wait to spend our first valentine's day together, and every other to come!`
-    },
 
     colors: {
         backgroundStart: "#E6E6FA",
         backgroundEnd: "#D8BFD8",
         buttonBackground: "#6A0DAD",
-        buttonHover: "#5A0099",
+        buttonHover: "#5A009D",
         textColor: "#4B0082"
     }
+
 };
 
 window.VALENTINE_CONFIG = CONFIG;
