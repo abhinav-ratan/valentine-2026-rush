1b388e869edf59198fd0a96d328792653bb57aec
Abhinav <137620031+abhinav-ratan@users.noreply.github.com>
2026-02-11 03:23:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
-const CONFIG = {
+const CONFIG={
 
-    valentineName: "Rashmi",
+valentineName:"Rashmi",
 
-    pageTitle: "Will you be my valentine?",
+pageTitle:"Will you be my valentine?",
 
-    colors: {
-        backgroundStart: "#E6E6FA",
-        backgroundEnd: "#D8BFD8",
-        buttonBackground: "#6A0DAD",
-        buttonHover: "#5A009D",
-        textColor: "#4B0082"
-    }
+colors:{
+backgroundStart:"#E6E6FA",
+backgroundEnd:"#D8BFD8",
+buttonBackground:"#6A0DAD",
+buttonHover:"#5A009D",
+textColor:"#4B0082"
+}
 
 };
 
-window.VALENTINE_CONFIG = CONFIG;
+window.VALENTINE_CONFIG=CONFIG;
